215504d586195573bfaf38bfa6f714eee192dee8
Victor Guedes <victorguedes68@hotmail.com>
2020-04-11 18:05:11 -0300

    New style

--- Desktop/src/App.js
+++ Desktop/src/App.js
@@ -4,37 +4,48 @@ import './style.css'
 function App(){
   return (
     <div className="container">
-        <div className="child-container">
-          <div id="screen">
-            <div>
-              <div id="answer">Text</div>
-            </div>
-          </div>
+      <div className="child-container">
 
-          <div>
+        <div id="screen">
+          <div className="teste">
+            <div id="answer">Text</div>
+          </div>
+        </div>
+
+        <Handlers/>          
+      </div>
+    </div>
+  )
+}
+
+function Handlers() {
+  return (
+    <div id="buttons">
+        <div className="teste">
             <p>1</p>
             <p>2</p>
             <p>3</p>
-          </div>
+        </div>
 
-          <div>
+        <div className="teste">
             <p>4</p>
             <p>5</p>
             <p>6</p>
-          </div>
+        </div>
 
-          <div>
+        <div className="teste">
             <p>7</p>
             <p>8</p>
             <p>9</p>
-          </div>
-
-          <div id="enviar">
-            <p>Enviar</p>
-          </div>
         </div>
-      </div>
-  )
+
+        <div id="send" className="teste">
+            <p>=</p>
+        </div>
+    </div>
+  );
 }
+
+
 
 export default App;

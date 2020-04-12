33a5da2883cf8d997ccf263fb68ae6314f15232d
Victor Guedes <victorguedes68@hotmail.com>
2020-04-12 13:09:57 -0300

    Inline functions changed

--- Desktop/src/App.js
+++ Desktop/src/App.js
@@ -4,9 +4,8 @@ import './style.css'
 function App(){
   const [val,setVal] = useState('');
 
-  function handleClick(e,value){
+  function handleClick(value){
     setVal(value);
-    
   }
   
   return (
@@ -21,24 +20,24 @@ function App(){
 
         <div id="buttons">
           <div className="teste">
-              <p onClick={() => setVal("1")}>1</p>
-              <p onClick={() => setVal("2")}>2</p>
-              <p onClick={() => setVal("3")}>3</p>
-              <p onClick={() => setVal("+")}>+</p>
+              <p onClick={() => handleClick("1")}>1</p>
+              <p onClick={() => handleClick("2")}>2</p>
+              <p onClick={() => handleClick("3")}>3</p>
+              <p onClick={() => handleClick("+")}>+</p>
           </div>
 
           <div className="teste">
-              <p onClick={() => setVal("4")}>4</p>
-              <p onClick={() => setVal("5")}>5</p>
-              <p onClick={() => setVal("6")}>6</p>
-              <p onClick={() => setVal("-")}>-</p>
+              <p onClick={() => handleClick("4")}>4</p>
+              <p onClick={() => handleClick("5")}>5</p>
+              <p onClick={() => handleClick("6")}>6</p>
+              <p onClick={() => handleClick("-")}>-</p>
           </div>
 
           <div className="teste">
-              <p onClick={() => setVal("7")}>7</p>
-              <p onClick={() => setVal("8")}>8</p>
-              <p onClick={() => setVal("9")}>9</p>
-              <p onClick={() => setVal("/")}>/</p>
+              <p onClick={() => handleClick("7")}>7</p>
+              <p onClick={() => handleClick("8")}>8</p>
+              <p onClick={() => handleClick("9")}>9</p>
+              <p onClick={() => handleClick("/")}>/</p>
           </div>
 
           <div id="send" className="teste">

43d66a1510d864fa1ce6cfd23887725aafe24f0e
Victor Guedes <victorguedes68@hotmail.com>
2020-04-12 13:05:00 -0300

    Interface updated

--- Desktop/src/App.js
+++ Desktop/src/App.js
@@ -21,21 +21,24 @@ function App(){
 
         <div id="buttons">
           <div className="teste">
-              <p onClick={() => setVal(1)}>1</p>
-              <p onClick={() => setVal(2)}>2</p>
-              <p onClick={() => setVal(3)}>3</p>
+              <p onClick={() => setVal("1")}>1</p>
+              <p onClick={() => setVal("2")}>2</p>
+              <p onClick={() => setVal("3")}>3</p>
+              <p onClick={() => setVal("+")}>+</p>
           </div>
 
           <div className="teste">
-              <p onClick={() => setVal(4)}>4</p>
-              <p onClick={() => setVal(5)}>5</p>
-              <p onClick={() => setVal(6)}>6</p>
+              <p onClick={() => setVal("4")}>4</p>
+              <p onClick={() => setVal("5")}>5</p>
+              <p onClick={() => setVal("6")}>6</p>
+              <p onClick={() => setVal("-")}>-</p>
           </div>
 
           <div className="teste">
-              <p onClick={() => setVal(7)}>7</p>
-              <p onClick={() => setVal(8)}>8</p>
-              <p onClick={() => setVal(9)}>9</p>
+              <p onClick={() => setVal("7")}>7</p>
+              <p onClick={() => setVal("8")}>8</p>
+              <p onClick={() => setVal("9")}>9</p>
+              <p onClick={() => setVal("/")}>/</p>
           </div>
 
           <div id="send" className="teste">

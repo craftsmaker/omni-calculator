d2b4b6b470cc0e44bafca9f56eff37ed56034e7b
Victor Guedes <victorguedes68@hotmail.com>
2020-04-16 12:14:28 -0300

    Functions eraseLastDigit and clearScreen added

--- Mobile/App.js
+++ Mobile/App.js
@@ -1,30 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, View, Text, TouchableOpacity} from "react-native";
 
 export default function App(){
+  const [value, setValue] = useState("");
+
+  function handlePress(valueFKey){
+
+    setValue(value + valueFKey);
+  }
+
+  function clearScreen(){
+    setValue("");
+  }
+
+  function eraseLastDigit(){
+    if(value){
+      setValue(value.substring(0,value.length - 1))
+    }
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.childContainer}>
         <View style={styles.screen}>
           <Text style={styles.text}>
-            Info
+            Info: {value}
           </Text>
         </View>
         <View style={styles.buttons}>
           <View style={styles.button}>
-            <TouchableOpacity style={styles.btn}><Text>1</Text></TouchableOpacity>
-            <TouchableOpacity style={styles.btn}><Text>2</Text></TouchableOpacity>
-            <TouchableOpacity style={styles.btn}><Text>3</Text></TouchableOpacity>
+            <TouchableOpacity onPress={() => handlePress("1")} style={styles.btn}><Text>1</Text></TouchableOpacity>
+            <TouchableOpacity onPress={() => handlePress("2")} style={styles.btn}><Text>2</Text></TouchableOpacity>
+            <TouchableOpacity onPress={() => handlePress("3")} style={styles.btn}><Text>3</Text></TouchableOpacity>
           </View>
           <View style={styles.button}>
-            <TouchableOpacity style={styles.btn}><Text>4</Text></TouchableOpacity>
-            <TouchableOpacity style={styles.btn}><Text>5</Text></TouchableOpacity>
-            <TouchableOpacity style={styles.btn}><Text>6</Text></TouchableOpacity>
+            <TouchableOpacity onPress={() => handlePress("4")} style={styles.btn}><Text>4</Text></TouchableOpacity>
+            <TouchableOpacity onPress={() => handlePress("5")} style={styles.btn}><Text>5</Text></TouchableOpacity>
+            <TouchableOpacity onPress={() => handlePress("6")} style={styles.btn}><Text>6</Text></TouchableOpacity>
           </View>
            <View style={styles.button}>
-            <TouchableOpacity style={styles.btn}><Text>4</Text></TouchableOpacity>
-            <TouchableOpacity style={styles.btn}><Text>5</Text></TouchableOpacity>
-            <TouchableOpacity style={styles.btn}><Text>6</Text></TouchableOpacity>
+            <TouchableOpacity onPress={() => handlePress("7")} style={styles.btn}><Text>4</Text></TouchableOpacity>
+            <TouchableOpacity onPress={() => handlePress("8")} style={styles.btn}><Text>5</Text></TouchableOpacity>
+            <TouchableOpacity onPress={() => handlePress("9")} style={styles.btn}><Text>6</Text></TouchableOpacity>
+          </View>
+           <View style={styles.button}>
+            <TouchableOpacity style={styles.btn}><Text>+</Text></TouchableOpacity>
+            <TouchableOpacity onPress={() => eraseLastDigit("8")} style={styles.btn}><Text>back</Text></TouchableOpacity>
+            <TouchableOpacity onPress={() => clearScreen()} style={styles.btn}><Text>C</Text></TouchableOpacity>
           </View>
 
         </View>
